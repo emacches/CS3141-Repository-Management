02de6c1ad0b37f1df629ed4babcc206a7ff7860f
emacches <emacches@mtu.edu>
2024-10-01 17:20:11 -0400

    Update CS3141Github.java: switches the order of words displayed

--- CS3141Github.java
+++ CS3141Github.java
@@ -2,7 +2,7 @@ class CS3141Github
 {
 	public static void main(String []args)
 	{
-		System.out.println("Hello world!");
 		System.out.println("This is an example of a java file.");
+		System.out.println("Hello world!");
 	}
 }
